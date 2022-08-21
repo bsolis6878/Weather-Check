5664e8a32d83702a848d5349176d3f65846c2c57
bsolis6878 <bsolis6878@gmail.com>
2022-08-21 16:51:02 -0500

    search button now console logs entered city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+var citySearch = document.querySelector("button");
+var city = document.querySelector("input");
+
+citySearch.addEventListener("click", function() {
+    var cityName = city.value;
+    weatherData(cityName);
+    forecastData(cityName);
+})
+
+
+
 var weatherData = function(location) {
      var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&APPID=5a42b591b4bd6478adc5120147487428"
 
